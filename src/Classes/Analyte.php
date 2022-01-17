7ec4dd3571fc9280b14074ead94527c2fdd84557
Bobby Hines <bobby@conflabs.com>
2022-01-16 17:32:05 -0800

    Analyte class modified to remove UUID references.
    Signed-off-by: Bobby Hines <bobby@conflabs.com>

--- src/Classes/Analyte.php
+++ src/Classes/Analyte.php
@@ -8,13 +8,12 @@ class Analyte
 
     /**
      * @param string $ulid
-     * @param ?string $uuid
      * @param string $scientific_name
      * @param array $common_names
      * @param string $category
      * @param string|null $cas_rn
      */
     public function __construct(
-      public string $ulid, public ?string $uuid, public string $scientific_name, public array $common_names, public string $category, public ?string $cas_rn
+      public string $ulid, public string $scientific_name, public array $common_names, public string $category, public ?string $cas_rn
     ) {}
 }
